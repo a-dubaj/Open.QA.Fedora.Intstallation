58f0ca6df5c5941cd4dfbfd329df86f25edd27b2
coffeina <dubaj.andrzej@gmail.com>
2023-01-18 04:01:42 +0100

    feat: update

--- scripts/install-openqa-post.sh
+++ scripts/install-openqa-post.sh
@@ -11,7 +11,7 @@ git clone https://pagure.io/fedora-qa/createhdds.git /root/createhdds
 sudo mkdir -p /var/lib/openqa/factory/hdd/fixed
 sudo mkdir -p /var/lib.openqa/factory/iso/
 
-echo "Cloning job from Fedora Project"
+echo "Clone job from Fedora Project"
 sudo open-clone-job ---from https://openqa.fedoraproject.org/tests/929283
 sudo systemctl enable --now openqa-worker@1
 echo "Scheduled job should be started by worker"
